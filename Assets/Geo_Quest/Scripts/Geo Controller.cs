1f4f8ad158c88eecef71cc0ce235a50b704e14c5
Tsion <78746596+NotMah@users.noreply.github.com>
2025-10-04 12:28:13 -0400

    E

--- Assets/Geo_Quest/Scripts/Geo Controller.cs
+++ Assets/Geo_Quest/Scripts/Geo Controller.cs
@@ -1,22 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GeoController : MonoBehaviour
 {
-    public string variableOne = "Hello";
-    string variableTwo = "World";
+    public string variable1 = "Hello";
+    string variable2 = "World";
+    private int var1 = 51;
+    private Rigidbody2D rb;
+    public int speed = 5;
 
     // Start is called before the first frame update
     void Start()
-    {
-        Debug.Log("Hello World");
-        Debug.Log(variableOne+variableTwo);
-    }
 
+    {
+       // string variable2 = "World";
+        Debug.Log(variable2);
+
+        Debug.Log("Hello World");
+        Debug.Log(variable1 + variable2);
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
     // Update is called once per frame
     void Update()
-    {
         
+    {
+     
+       // rb.velocity = new Vector2(-1,rb.velocity.y);
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2 (xInput* speed, rb.velocity.y);
+        Debug.Log(xInput);
+
+        /*
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+            
+        {
+            transform.position += new Vector3(1, 0, 0);
+
+
+        } 
+      */
     }
 }
