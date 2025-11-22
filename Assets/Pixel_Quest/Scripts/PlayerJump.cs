0f222d7c5ebb784bccf073bc406467d4df3343ab
Tsion <78746596+NotMah@users.noreply.github.com>
2025-11-22 16:55:50 -0500

    EEE

--- Assets/Pixel_Quest/Scripts/PlayerJump.cs
+++ Assets/Pixel_Quest/Scripts/PlayerJump.cs
@@ -12,38 +12,46 @@ public class PlayerJump : MonoBehaviour
     public LayerMask groundMask;
     private bool _groundCheck;
     private Animator animator;
-
+    public float fallForce = 2;
+    private Vector2 _gravityVector;
+    public float _rigidbody2D;
+    public float velocity;
     // Start is called before the first frame update
     void Start()
     {
+        _gravityVector = new Vector2(0, Physics2D.gravity.y);
         _Rigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponentInChildren<Animator>();    
     }
 
     // Update is called once per frame
     void Update()
-    {
-      if (Input.GetKeyDown(KeyCode.Space))
-        {
-            _Rigidbody2D.velocity = new Vector2(_Rigidbody2D.velocity.x, jumpForce);
-            //animator.SetBool("isjumping", true);
-            //animator.SetBool("iswalking", false);
-        }  
-      /*
-      else
-        {
-            animator.SetBool("isjumping", false);
-        }
-      */
 
-      if (_Rigidbody2D.velocity.y < 0)
+    {
+        _groundCheck = Physics2D.OverlapCapsule(feetCollider.position, new Vector2(CapsulelHeight, CapsuleRadius), CapsuleDirection2D.Horizontal, 0, groundMask);
+      if (Input.GetKeyDown(KeyCode.Space) &&  _groundCheck)
         {
             animator.SetBool("isjumping", true);
-            animator.SetBool("iswalking", false);
+            _Rigidbody2D.velocity = new Vector2(_Rigidbody2D.velocity.x, jumpForce);
+      
+        }
+
+
+        if (_Rigidbody2D.velocity.y > 0)
+        {
+   
+            _Rigidbody2D.velocity += _gravityVector * (fallForce * Time.deltaTime);
         }
         else
         {
             animator.SetBool("isjumping", false);
         }
+       
+
+
+
+
+
+
     }
 }
