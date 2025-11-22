e3776f5c1dbaa8d2d0fac6e0b96de4a255b99b9b
Tsion <78746596+NotMah@users.noreply.github.com>
2025-11-22 11:56:25 -0500

    EE

--- Assets/Pixel_Quest/Scripts/PlayerJump.cs
+++ Assets/Pixel_Quest/Scripts/PlayerJump.cs
@@ -11,11 +11,13 @@ public class PlayerJump : MonoBehaviour
     public Transform feetCollider;
     public LayerMask groundMask;
     private bool _groundCheck;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         _Rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();    
     }
 
     // Update is called once per frame
@@ -24,6 +26,24 @@ public class PlayerJump : MonoBehaviour
       if (Input.GetKeyDown(KeyCode.Space))
         {
             _Rigidbody2D.velocity = new Vector2(_Rigidbody2D.velocity.x, jumpForce);
+            //animator.SetBool("isjumping", true);
+            //animator.SetBool("iswalking", false);
         }  
+      /*
+      else
+        {
+            animator.SetBool("isjumping", false);
+        }
+      */
+
+      if (_Rigidbody2D.velocity.y < 0)
+        {
+            animator.SetBool("isjumping", true);
+            animator.SetBool("iswalking", false);
+        }
+        else
+        {
+            animator.SetBool("isjumping", false);
+        }
     }
 }
--- Assets/Pixel_Quest/Scripts/PlayerMovement.cs
+++ Assets/Pixel_Quest/Scripts/PlayerMovement.cs
@@ -7,11 +7,13 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D _rigidbody2D;
     public int speed = 4;
     private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
     // Start is called before the first frame update
     void Start()
     {
         _spriteRenderer = transform.GetComponentInChildren<SpriteRenderer>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponentInChildren<Animator>();   
     }
 
     // Update is called once per frame
@@ -20,9 +22,17 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         _rigidbody2D.velocity = new Vector2(horizontal * speed, _rigidbody2D.velocity.y);
 
-        if (horizontal > 0) { _spriteRenderer.flipX = true; }
-        else if (horizontal < 0) { _spriteRenderer.flipX = false;}
+        if (horizontal > 0) { _spriteRenderer.flipX = false; }
+        else if (horizontal < 0) { _spriteRenderer.flipX = true;}
+        if (horizontal != 0)
+        {
+            _animator.SetBool("iswalking", true);
 
+        }
+        else
+        {
+            _animator.SetBool("iswalking", false);
+        }
         //if (Input.GetKeyDown(KeyCode.Space) && _groundC
 
     }
